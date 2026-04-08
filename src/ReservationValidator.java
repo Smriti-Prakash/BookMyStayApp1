19947053bff391acde7b47d762b3f03ce775fcea
Smriti-Prakash <smriti.prakash012@gmail.com>
2026-04-08 16:20:43 +0530

    UC:9 Error Handling & Validation

---
+++ src/ReservationValidator.java
@@ -0,0 +1,40 @@
+/**
+ * ===============================================================
+ * CLASS - ReservationValidator
+ * ===============================================================
+ * Use Case 9: Error Handling & Validation
+ *
+ * @version 9.0
+ */
+public class ReservationValidator {
+
+    public void validate(
+            String guestName,
+            String roomType,
+            RoomInventory inventory
+    ) throws InvalidBookingException {
+
+        // Validate guest name
+        if (guestName == null || guestName.trim().isEmpty()) {
+            throw new InvalidBookingException("Guest name cannot be empty.");
+        }
+
+        // Validate room type (case-sensitive as per requirement)
+        if (!roomType.equals("Single") &&
+                !roomType.equals("Double") &&
+                !roomType.equals("Suite")) {
+
+            throw new InvalidBookingException("Invalid room type selected.");
+        }
+
+        // Validate availability
+        int available = inventory.getRoomAvailability()
+                .getOrDefault(roomType, 0);
+
+        if (available <= 0) {
+            throw new InvalidBookingException(
+                    "No rooms available for selected type."
+            );
+        }
+    }
+}
